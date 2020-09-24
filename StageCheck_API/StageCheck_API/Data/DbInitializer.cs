300a032a20a66a372ed007a7a4d157c39f25277c
dennycox <denny.cox@chello.nl>
2020-09-24 16:32:09 +0200

    Added basic model, controller, context and database initializer

---
+++ StageCheck_API/StageCheck_API/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using StageCheck_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StageCheck_API.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(StageCheckContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Internships.Any())
+            {
+                return;
+            }
+
+            var internships = new Internship[]
+            {
+                new Internship{Title="ICT hbo afstudeerstage",
+                    Description="Hele leuke en uitdagende stageopdracht", CompanyId="1",
+                }
+            };
+            foreach (Internship i in internships)
+            {
+                context.Internships.Add(i);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ StageCheck_API/StageCheck_API/Data/StageCheckContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StageCheck_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StageCheck_API.Data
+{
+    public class StageCheckContext : DbContext
+    {
+        public StageCheckContext(DbContextOptions<StageCheckContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Internship> Internships { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Internship>().ToTable("Internship");
+        }
+    }
+}
