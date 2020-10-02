51e5bed6526e2cb73ada94cc6dc1a36a857ed256
dennycox <denny.cox@chello.nl>
2020-10-02 15:58:14 +0200

    Changed classes to functions

--- stagecheck_web_app/src/components/Banner.js
+++ stagecheck_web_app/src/components/Banner.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Banner extends Component {
-    render() {
-        return (
-            <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">StageCheck</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#internships">Alle stages</Nav.Link>
-                    <Nav.Link href="#companies">Bedrijven</Nav.Link>
-                    <Nav.Link href="#schools">Scholen</Nav.Link>
-                    <Nav.Link href="#students">Studenten</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Zoeken" className="mr-sm-2" />
-                    <Button variant="outline-primary">Stage zoeken</Button>
-                </Form>
-            </Navbar>
-        )
-    }
+function Banner() {
+    return (
+        <Navbar bg="light" variant="light">
+            <Navbar.Brand href="#home">StageCheck</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#internships">Alle stages</Nav.Link>
+                <Nav.Link href="#companies">Bedrijven</Nav.Link>
+                <Nav.Link href="#schools">Scholen</Nav.Link>
+                <Nav.Link href="#students">Studenten</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Zoeken" className="mr-sm-2" />
+                <Button variant="outline-primary">Stage zoeken</Button>
+            </Form>
+        </Navbar>
+    )
 }
 
 export default Banner;
