44e2fae80e21eb4d37155799c199eaae159cf529
dennycox <denny.cox@chello.nl>
2020-10-08 14:13:22 +0200

    Improved add form styling

--- stagecheck_web_app/src/components/AddInternshipForm.js
+++ stagecheck_web_app/src/components/AddInternshipForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Form } from 'react-bootstrap';
 
 function AddInternshipForm({ addInternship }) {
   const [title, setTitle] = useState("");
@@ -19,21 +20,21 @@ function AddInternshipForm({ addInternship }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Titel"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      ></input>
-      <input
-        type="text"
-        placeholder="Beschrijving"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></input>
-      <button className="grey-button">Voeg stage toe</button>
-    </form>
+    <div class="row pt-5">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Titel</Form.Label>
+          <Form.Control type="text" placeholder="Voer titel in"
+            onChange={(e) => setTitle(e.target.value)} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Beschrijving</Form.Label>
+          <Form.Control type="text" placeholder="Voer beschrijving in"
+            onChange={(e) => setDescription(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit">Voeg stage toe</Button>
+      </Form>
+    </div>
   );
 }
 
