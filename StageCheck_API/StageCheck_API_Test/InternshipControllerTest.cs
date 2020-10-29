328dd5206c6a993eebdead33d6763b71dd480c24
dennycox <denny.cox@chello.nl>
2020-10-29 13:14:18 +0100

    Added integration tests for the internshipcontroller

---
+++ StageCheck_API/StageCheck_API_Test/InternshipControllerTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StageCheck_API_Test
+{
+    class InternshipControllerTest : IClassFixture<CustomWebApplicationFactory<Core_API.Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Core_API.Startup> _factory;
+
+        public InternshipControllerTest(
+        CustomWebApplicationFactory<Core_API.Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task Get_Retrieve_Internships()
+        {
+            var response = await _client.GetAsync("api/internships");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Get_Not_Retrieve_Internships()
+        {
+            var response = await _client.GetAsync("api/internship");
+
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task Get_Retrieve_One_Internship()
+        {
+            var response = await _client.GetAsync("api/internships/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Get_Not_Retrieve_One_Internship()
+        {
+            var response = await _client.GetAsync("api/internships/0");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task Post_Succeed_Internship()
+        {
+            var response = await _client.PostAsync("api/internships", new StringContent(JsonConvert.SerializeObject(new Internship()
+            {
+                Title = "New internship title",
+                Description = "New internship description"
+            }), Encoding.UTF8, "application/json"));
+
+            response.EnsureSuccessStatusCode();
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+
+        [Fact]
+        public async Task Put_Succeed_Internship()
+        {
+            var response = await _client.PutAsync("api/internships/1", new StringContent(JsonConvert.SerializeObject(new Internship()
+            {
+                Id = 1,
+                Title = "Updated internship title",
+                Description = "Updated internship description"
+            }), Encoding.UTF8, "application/json"));
+
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_Succeed_Internship()
+        {
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+
+                    using (var scope = serviceProvider.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices
+                            .GetRequiredService<StageCheckContext>();
+                        var logger = scopedServices
+                            .GetRequiredService<ILogger<StageCheck_API.Startup>>();
+
+                        try
+                        {
+                            Utilities.ReinitializeDbForTests(db);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.LogError(ex, "An error occurred seeding " +
+                                "the database with test messages. Error: {Message}",
+                                ex.Message);
+                        }
+                    }
+                });
+            })
+      .CreateClient(new WebApplicationFactoryClientOptions
+      {
+          AllowAutoRedirect = false
+      });
+
+            var response = await client.DeleteAsync("api/internships/1");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ StageCheck_API/StageCheck_API_Test/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StageCheck_API_Test
+{
+    class Utilities
+    {
+        public static void InitializeDbForTests(StageCheckContext db)
+        {
+            db.Internships.AddRange(GetSeedingMessages());
+
+            db.SaveChanges();
+        }
+
+        public static void ReinitializeDbForTests(StageCheckContext db)
+        {
+            db.Internships.RemoveRange(db.Internships);
+            InitializeDbForTests(db);
+        }
+        public static List<NewsArticle> GetSeedingMessages()
+        {
+            return new List<Internship>()
+            {
+             new Internship(){ ID = 1, Title = "ICT hbo afstudeerstage", Description = "Hele leuke en uitdagende afstudeeropdracht" },
+             new Internship(){ ID = 2, Title = "Bedrijfskunde hbo 2e jaars stage", Description = "2 stageplekken vrij" },
+             new Internship(){ ID = 3, Title = "ICT hbo stage 2e jaars", Description = "Uitdagende stageopdracht" }
+            };
+        }
+    }
+}
