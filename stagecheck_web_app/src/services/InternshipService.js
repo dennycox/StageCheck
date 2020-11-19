56650d8f5b3ff25a392a3d3cd8f7c986291f1383
dennycox <denny.cox@chello.nl>
2020-11-19 17:03:59 +0100

    Fixed api url

--- stagecheck_web_app/src/services/InternshipService.js
+++ stagecheck_web_app/src/services/InternshipService.js
@@ -3,23 +3,23 @@ import axios from 'axios';
 const api = "https://localhost:44330/api";
 
 const getAll = async (search = '', studyId = '') => {
-  return await axios.get(api,`/internships?search=${search}&studyId=${studyId}`);
+  return await axios.get(api + `/internships?search=${search}&studyId=${studyId}`);
 };
 
 const get = async id => {
-  return await axios.get(api,`/internships/${id}`);
+  return await axios.get(api + `/internships/${id}`);
 };
 
 const create = async data => {
-  return await axios.post(api,"/internships", data);
+  return await axios.post(api + "/internships", data);
 };
 
 const update = async (id, data) => {
-  return await axios.put(api,`/internships/${id}`, data);
+  return await axios.put(api + `/internships/${id}`, data);
 };
 
 const remove = async id => {
-  return await axios.delete(api,`/internships/${id}`);
+  return await axios.delete(api + `/internships/${id}`);
 };
 
 export default {
