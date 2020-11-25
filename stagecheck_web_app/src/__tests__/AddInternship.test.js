2e8dbe48d344b7434c2f9669d1099dc8897f9f63
dennycox <denny.cox@chello.nl>
2020-11-25 16:07:30 +0100

    Worked on tests

--- stagecheck_web_app/src/__tests__/AddInternship.test.js
+++ stagecheck_web_app/src/__tests__/AddInternship.test.js
@@ -1,7 +1,13 @@
-import { getByTestId, render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { queryByTestId, render, screen, waitFor, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AddInternship from '../pages/AddInternship';
 import InternshipService from '../services/InternshipService';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+import "regenerator-runtime/runtime.js";
 
 jest.mock('../services/InternshipService');
 
@@ -11,18 +17,18 @@ describe("Add internships test", () => {
             console.log(data);
         });
 
-        render(<AddInternship />);
+        const { queryByTestId } = render(<MemoryRouter initialEntries={["/add"]}><AddInternship /></MemoryRouter>);
 
-        const inputTitle = screen.getByTestId("add-internship-title-input");
-        fireEvent.change(inputTitle, { target: { value: 'Test internship title' } });
+        const inputTitle = queryByTestId("add-internship-title-input");
+        fireEvent.change(inputTitle, { target: { value: "Test internship title" } });
         expect(inputTitle.value).toBe("Test internship title");
 
-        const inputDescription = screen.getByTestId("add-internship-description-input");
-        fireEvent.change(inputDescription, { target: { value: 'Test internship description' } });
+        const inputDescription = queryByTestId("add-internship-description-input");
+        fireEvent.change(inputDescription, { target: { value: "Test internship description" } });
         expect(inputDescription.value).toBe("Test internship description");
 
-        const submit = screen.getByTestId("add-internship-submit")
+        const submit = queryByTestId("add-internship-submit")
         fireEvent.click(submit);
-        expect(mockFn).toHaveBeenCalledWith({ title: 'Test internship title', description: 'Test internship description' });
+        expect(mockFn).toHaveBeenCalledWith({ title: "Test internship title", description: "Test internship description" });
     });
 }); 
