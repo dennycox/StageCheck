4611e2d66b1a9cc74dc4aab1e40c97e8a1dff6e9
dennycox <denny.cox@chello.nl>
2020-11-26 11:07:47 +0100

    Company info now shows in internship list

--- stagecheck_web_app/src/components/Company.js
+++ stagecheck_web_app/src/components/Company.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 import CompanyService from '../services/CompanyService'
+import { MdLocationOn } from "react-icons/md";
 
 const Company = ({ companyId }) => {
     const [company, setCompany] = useState([]);
@@ -23,7 +24,7 @@ const Company = ({ companyId }) => {
         <Card className="my-3">
             <Card.Body>
                 <Card.Title>{company.name}</Card.Title>
-                <Card.Text>{company.streetName} {company.houseNumber} {company.houseNumberAddition}, {company.zipCode} {company.city}</Card.Text>
+                <Card.Text><MdLocationOn color="red" /> {company.streetName} {company.houseNumber} {company.houseNumberAddition}, {company.zipCode} {company.city}</Card.Text>
                 <Card.Text>{company.webSite}</Card.Text>
                 <Card.Text>{company.phoneNumber}</Card.Text>
             </Card.Body>
--- stagecheck_web_app/src/components/Internship.js
+++ stagecheck_web_app/src/components/Internship.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import CompanyService from '../services/CompanyService';
+import { MdLocationOn } from "react-icons/md";
 
 const Internship = ({ internship, ...props }) => {
+
+    const [company, setCompany] = useState([]);
+
+    useEffect(() => {
+        getCompany(internship.companyId);
+    }, [internship.companyId]);
+
+    const getCompany = id => {
+        CompanyService.get(id)
+            .then(response => {
+                setCompany(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
 
     return (
         <Card className="my-3" data-testid="card" {...props}>
             <Card.Body>
-                <Card.Title>{internship.title}</Card.Title>
-                <Card.Text>{internship.description}</Card.Text>
+                <Card.Title>{company.name}</Card.Title>
+                <Card.Text>{internship.title}</Card.Text>
+                <Card.Text><MdLocationOn color="red"/> {company.streetName} {company.houseNumber} {company.houseNumberAddition}, {company.zipCode} {company.city}</Card.Text>
                 <Link to={"/details/" + internship.id}>Meer informatie</Link>
             </Card.Body>
         </Card>
